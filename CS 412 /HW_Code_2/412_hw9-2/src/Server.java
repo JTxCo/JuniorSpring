bb4ab006d441ab472d3876e574b89ee9ead40c98
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-03-06 11:39:32 -0700

    412CommitHw-9.25

--- CS 412 /HW_Code_2/412_hw9-2/src/Server.java
+++ CS 412 /HW_Code_2/412_hw9-2/src/Server.java
@@ -1,25 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.Buffer;
 
 public class Server {
-    private ServerSocket server;
-    private Socket conn;
-    private BufferedReader reader;
-    private PrintWriter writer;
+//    private ServerSocket server;
+//    private Socket conn;
+//    private BufferedReader reader;
+//    private PrintWriter writer;
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         try {
-            server = new ServerSocket(5000);
+            ServerSocket server = new ServerSocket(5000);
             while(true) {
-                conn = server.accept();
-                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                writer = new PrintWriter(conn.getOutputStream());
-                writer.println(("you gave me: " + reader));
+               Socket conn = server.accept();
+               BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+               PrintWriter writer = new PrintWriter(conn.getOutputStream());
+               writer.println(("you gave me: " + reader));
+
             }
         } catch (IOException e) {
             e.printStackTrace();
