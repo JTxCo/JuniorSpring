892756dbf8bb8ca42e834819d7d88741ca1a32b5
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-03-06 11:33:53 -0700

    412CommitHw-9.25

--- CS 412 /HW_Code_2/412_hw9-2/src/Client.java
+++ CS 412 /HW_Code_2/412_hw9-2/src/Client.java
@@ -1,3 +1,24 @@
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class Client {
+    private static PrintWriter pr;
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 5000);
+            pr = new PrintWriter(socket.getOutputStream());
+            pr.println("input: ");
+            pr.flush();
+            InputStreamReader stream = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(stream);
+            String message = reader.readLine();
+            System.out.println("stuff got back is: " + message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- CS 412 /HW_Code_2/412_hw9-2/src/Server.java
+++ CS 412 /HW_Code_2/412_hw9-2/src/Server.java
@@ -1,2 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
 public class Server {
+    private ServerSocket server;
+    private Socket conn;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public void main(String[] args) {
+        try {
+            server = new ServerSocket(5000);
+            while(true) {
+                conn = server.accept();
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                writer = new PrintWriter(conn.getOutputStream());
+                writer.println(("you gave me: " + reader));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
